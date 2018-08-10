6064ed207c0d5a32b43677297db8c31f913625d7
leensungzero <brianwoo119@naver.com>
2018-08-10 22:48:33 +0900

    루트 페이지 만들기/url routing

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title="Todo API", description="RESTful API for Todo")),
+
+    path('', views.api_root),
+    path('', include('users.urls', namespace='users')),
+    path('', include('todos.urls', namespace='todos')),
 ]
